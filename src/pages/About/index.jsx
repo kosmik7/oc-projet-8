01db9b994eda3abf7d4ae903b375430cfd00ec2f
kosmik7 <34013885+kosmik7@users.noreply.github.com>
2023-03-02 16:43:29 +0100

    Ajout de la page 'A Propos'

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,9 +1,35 @@
-function About() {
+import React from 'react'
+import styled from 'styled-components'
+
+import { Banner, Collapse } from '../../components'
+import { device, BaseContainer } from '../../styles/global'
+import bannerImage from '../../assets/banner-about.webp'
+import data from '../../assets/about.json'
+
+const Container = styled(BaseContainer)`
+    margin-top: 22px;
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+
+    @media ${device.medium} {
+        margin-top: 42px;
+        gap: 30px;
+    }
+`
+export default function Home() {
     return (
-        <div>
-            <h1> Page A-propos 🏠</h1>
-        </div>
+        <React.Fragment>
+            <Banner image={bannerImage} imageAlt="Un paysage montagneux" />
+            <Container mediumWidth>
+                {data.map((item) => (
+                    <Collapse
+                        key={item.id}
+                        title={item.title}
+                        description={item.description}
+                    />
+                ))}
+            </Container>
+        </React.Fragment>
     )
 }
-
-export default About
