298a9ec7534cc5a6dc35aea40a66680e79b54eff
kosmik7 <34013885+kosmik7@users.noreply.github.com>
2023-03-07 12:17:29 +0100

    Ajout du composant 'Slideshow'

---
+++ src/components/Slideshow/index.jsx
@@ -0,0 +1,125 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+import PropTypes from 'prop-types'
+import { device, BaseContainer } from '../../styles/global'
+import { ReactComponent as IcoArrow } from '../../assets/arrow.svg'
+
+const Wrapper = styled(BaseContainer)`
+    margin-top: 22px;
+
+    @media ${device.small} {
+        margin-top: 42px;
+    }
+`
+const Container = styled.div`
+    position: relative;
+`
+
+const Image = styled.img`
+    width: 100%;
+    height: 255px;
+    object-fit: cover;
+
+    border-radius: 25px;
+    overflow: hidden;
+
+    @media ${device.small} {
+        height: 355px;
+        margin-top: 42px;
+        border-radius: 10px;
+    }
+
+    @media ${device.medium} {
+        height: 415px;
+    }
+`
+
+const Arrow = styled(IcoArrow)`
+    width: 24px;
+    height: 24px;
+    position: absolute;
+    top: 50%;
+    z-index: 1;
+    filter: drop-shadow(3px 2px 3px rgb(0 0 0 / 0.25));
+
+    @media ${device.small} {
+        width: 48px;
+        height: 48px;
+    }
+
+    @media ${device.medium} {
+        width: 96px;
+        height: 96px;
+    }
+`
+
+const ArrowRight = styled(Arrow)`
+    right: 0;
+    transform: translate(0, -50%);
+`
+
+const ArrowLeft = styled(Arrow)`
+    left: 0;
+    transform: scaleX(-1) translate(0, -50%);
+`
+
+const Counter = styled.p`
+    position: absolute;
+    bottom: 5%;
+    right: 50%;
+    transform: translate(50%, 0);
+
+    font-size: 18px;
+    color: #fff;
+    text-shadow: 3px 2px 3px rgb(0 0 0 / 0.4);
+`
+
+Slideshow.propTypes = {
+    pictures: PropTypes.array,
+}
+
+export default function Slideshow({ pictures }) {
+    const multiple = pictures.length <= 1 ? false : true
+    const [currentIndex, setCurrentIndex] = useState(0)
+
+    return (
+        <Wrapper>
+            <Container>
+                <Image src={pictures[currentIndex]} />
+                {multiple && (
+                    <React.Fragment>
+                        <Link>
+                            <ArrowRight
+                                src={IcoArrow}
+                                onClick={
+                                    currentIndex === pictures.length - 1
+                                        ? () => setCurrentIndex(0)
+                                        : () =>
+                                              setCurrentIndex(currentIndex + 1)
+                                }
+                            />
+                        </Link>
+                        <Link>
+                            <ArrowLeft
+                                src={IcoArrow}
+                                onClick={
+                                    currentIndex === 0
+                                        ? () =>
+                                              setCurrentIndex(
+                                                  pictures.length - 1
+                                              )
+                                        : () =>
+                                              setCurrentIndex(currentIndex - 1)
+                                }
+                            />
+                        </Link>
+                        <Counter>
+                            {currentIndex + 1}/{pictures.length}
+                        </Counter>
+                    </React.Fragment>
+                )}
+            </Container>
+        </Wrapper>
+    )
+}
--- src/pages/Logement/index.jsx
+++ src/pages/Logement/index.jsx
@@ -1,2 +1,15 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+import { Slideshow } from '../../components'
+import data from '../../assets/logements.json'
+
 export default function Logement() {
+    const { id } = useParams()
+    const logement = data.find((item) => item.id === id)
+
+    return (
+        <div>
+            <Slideshow pictures={logement.pictures} />
+        </div>
+    )
 }
